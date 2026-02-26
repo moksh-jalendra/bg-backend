8477b8eb13a0df9b1e1afc040ee6cf889bc2a161
moksh jalendra <mokshjalendra33@gmail.com>
2026-02-26 13:10:16 +0530

    l model error soled commit

--- bg.py
+++ bg.py
@@ -1,7 +1,7 @@
 from withoutbg import WithoutBG
-
+model = WithoutBG.opensource()
 
 def bg_remove(z):
-    model = WithoutBG.opensource()
+    
     resut = model.remove_background(z)
     resut.save('photo-withoutbg.png')
